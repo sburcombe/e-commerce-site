7ac94fd87d5f2bf7788b075a1c952e8a1d952f19
Stephanie B <sburcombe@gmail.com>
2019-07-12 16:50:00 +0000

    Define a stateful ProductDetails component in client/components/product-details.jsx with an initial state that includes a product set to null and an initial render method that returns null.

---
+++ client/components/product-details.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default ProductDetails;

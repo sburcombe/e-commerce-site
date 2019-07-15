a90624d54d26f40350bbe2c77b4955f0009fe401
Stephanie B <sburcombe@gmail.com>
2019-07-15 23:28:14 +0000

    Created Connection File for MYSQL

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,13 +1,14 @@
 <?php
 
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 
 // if (empty($_GET['id'])) {
 //   readfile('dummy-products-list.json');
 // } else {
 //   readfile('dummy-product-details.json');
 // }
-require('functions.php');
+require_once('db_connection.php');
+require_once('functions.php');
 set_exception_handler('error_handler');
 // doStuff();
 $output = file_get_contents('dummy-products-list.json');

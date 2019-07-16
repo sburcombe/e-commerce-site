e27e96e92179ff1045c07562f933186a88550cc4
Stephanie B <sburcombe@gmail.com>
2019-07-16 00:17:39 +0000

    updated formatting

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,19 +1,16 @@
 <?php
 
 header('Content-Type: application/json');
+require_once('functions.php');
+set_exception_handler('error_handler');
 
+require_once('db_connection.php');
+$output = file_get_contents('dummy-products-list.json');
+print($output);
 // if (empty($_GET['id'])) {
 //   readfile('dummy-products-list.json');
 // } else {
 //   readfile('dummy-product-details.json');
 // }
-require_once('functions.php');
-set_exception_handler('error_handler');
-require_once('db_connection.php');
-
-// doStuff();
-$output = file_get_contents('dummy-products-list.json');
-print($output);
-
 
 ?>

c7d94e139edbce3099874e6392d3c7f4494ec344
Stephanie B <sburcombe@gmail.com>
2019-07-16 00:05:32 +0000

    Fixed order of require files

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -7,9 +7,10 @@ header('Content-Type: application/json');
 // } else {
 //   readfile('dummy-product-details.json');
 // }
-require_once('db_connection.php');
 require_once('functions.php');
 set_exception_handler('error_handler');
+require_once('db_connection.php');
+
 // doStuff();
 $output = file_get_contents('dummy-products-list.json');
 print($output);

82b05082fa5e6a0e31d3f42b641acab3f1ff2115
Stephanie B <sburcombe@gmail.com>
2019-07-16 02:12:35 +0000

    created array variable looped through the data coming from the result via mysqli_fetch_assoc and pushed it into the output variable json encoded the output data and printed the json data tested data in postman added startup function with application/json header

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,8 +1,7 @@
 <?php
-header('Content-Type: application/json');
 require_once('functions.php');
 set_exception_handler('error_handler');
-
+startup();
 require_once('db_connection.php');
 
 $query = "SELECT * FROM products";
@@ -12,6 +11,10 @@ $result = mysqli_query($conn, $query);
 if (!$result){
   throw new Exception('error with query: ' . mysqli_error($conn));
 }
-// $output = file_get_contents('dummy-products-list.json');
-// print($output);
+$data = [];
+while($row = mysqli_fetch_assoc($result)){
+  $data[] = $row;
+}
+
+print(json_encode($data));
 ?>

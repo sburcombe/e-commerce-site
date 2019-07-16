1926b40f19b1672a9e4f7852223fbeac90d0192f
Stephanie B <sburcombe@gmail.com>
2019-07-16 01:25:08 +0000

    Created schema for products table created a database, wickedsales, in my phphmyadmin filled products table with dummy data : 2 products wrote query to test dummy data tested query on phpmyadmin

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,16 +1,17 @@
 <?php
-
 header('Content-Type: application/json');
 require_once('functions.php');
 set_exception_handler('error_handler');
 
 require_once('db_connection.php');
-$output = file_get_contents('dummy-products-list.json');
-print($output);
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
-// }
 
+$query = "SELECT * FROM products";
+$result = mysqli_query($conn, $query);
+
+
+if (!$result){
+  throw new Exception('error with query: ' . mysqli_error($conn));
+}
+// $output = file_get_contents('dummy-products-list.json');
+// print($output);
 ?>

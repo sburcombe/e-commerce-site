bc9849a545fac956f03d08064078819c0a1d50c4
Stephanie B <sburcombe@gmail.com>
2019-09-24 04:45:31 +0000

    finished adding the printing encoded json result for the cart_get.php file need to complete testing

--- server/public/api/cart_get.php
+++ server/public/api/cart_get.php
@@ -12,14 +12,29 @@ if(empty($_SESSION['cartId'])) {
 }
 
 $cartId = intval($_SESSION['cartId']);
-// $cartItemsQuery = //Select all from cartItems where cartId = cartId
+//Select all from cartItems where cartId = cartId
                   //Join cartitems with products on productID
                   //to get id, name, price, shortDescription
                   //subquery to get the first image from images table
                   //you'll need to only get the cart where the cart ID is the
                   //one you are looking for
+$cartItemsQuery = "SELECT cartItems.cartID, products.id, products.name, products.price, products.shortDescription,
+(SELECT img_url FROM images WHERE product_id = products.id LIMIT 1)AS images FROM products
+INNER JOIN cartItems ON cartItems.productID = products.id WHERE cartItems.cartID = $cartId";
 
 //Send the query to mysql and get the result
+ $cartItemsQueryResult = mysqli_query($conn, $cartItemsQuery);
+
 //Retrieve the data you got from the query and print it out.
 //If there is nothing there, make sure it prints out an empty array
+if(!$cartItemsQueryResult){
+  print(json_encode([]));
+} else{
+  print(json_encode($cartItemsQueryResult));
+}
+
+//left off with the final query testing accurately but not printing what I
+//am expecting.  Possibly has to do with the SESSION? Might need to test
+//differently.
+
 ?>
